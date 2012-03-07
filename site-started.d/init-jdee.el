d713bd5edb72d5cb699c3a54c18c325c2c570378
ned rihine <ned.rihine@gmail.com>
2012-03-07 23:19:13 +0900

    sr-speedbar.el を追加、cedet と jdee の設定を追加。

---
+++ site-started.d/init-jdee.el
@@ -0,0 +1,32 @@
+;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil; -*-
+
+;; init-jdee.el -- Emacs init setting elisp file
+
+;; Copyright (c) 2009-2012 ned rihine
+
+;; Author: ned rihine <ned.rihine@gmail.com>
+;; Keyword: tools
+
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+ 
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(setq defer-loading-jde nil)
+
+(if defer-loading-jde
+    (progn
+      (autoload 'jde-mode "jde" "JDE mode." t)
+      (push '("\\.java$" . jde-mode) auto-mode-alist)
+      (require-if-exists jde)))
+
+(provide 'init-jdee)
+;; init-jdee.el ends here
