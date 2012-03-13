15a1b161cb3b1c4e3273608e7963bcd8bb31bcd4
ned rihine <ned.rihine@gmail.com>
2012-03-13 13:44:24 +0900

    apel を削除して、eldoc-extension, c-eldoc, cc-defs を追加しました。

---
+++ site-started.d/init-eldoc.el
@@ -0,0 +1,34 @@
+;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil; -*-
+
+;; init-eldoc.el -- Emacs init setting elisp file
+
+;; Copyright (c) 2009-2012 ned rihine
+
+;; Author: ned rihine <ned.rihine@gmail.com>
+;; Keyword: tools
+
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+ 
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http:;;www.gnu.org/licenses/>.
+
+(require-if-exists eldoc)
+(require-if-exists eldoc-extension)
+
+(setq eldoc-idle-delay 130)
+(setq eldoc-echo-are-use-multiline-p t)
+
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+
+(provide 'init-eldoc)
+;; init-eldoc.el ends here
